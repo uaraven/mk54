a521a89e963260096e37cfea4c94cc33d507cfe2
Oleksii Voronin <ovoronin@gmail.com>
2018-07-27 21:20:10 -0400

    Added goto code generation.
    Added documentation on how various jumps will be handled

--- docs/goto.md
+++ docs/goto.md
@@ -4,11 +4,11 @@ GOTO support
 Being a machine code language MK-series allows unconditional jumps (called БП) in mnemonic language from any place to
 any place. БП command (called GOTO in the rest of this document) will change instruction pointer to the address specified
 as the next operation. Technically this allows to jump to 256 addresses, but MK calculators had only 98 to 105 bytes of
-program memory. 
+program memory. This address is referred to as *MK address* in this document.
 
 Of course when you writing program in machine code which must be no longer than 98 bytes you have to be very clever 
-with your code. MK programs are great examples of spaghetti-code. This causes challenges when transpiling MK code to
-byte code. 
+with your code. MK programs are great examples of spaghetti-code. This causes challenges when generating JVM byte code
+from MK code.
 
 As `mk54` generates byte code, it keeps a list of labels (used by ASM to generate correct offsets for jump commands) for
 each MK operation. When GOTO operation is encountered it can easily be compiled to byte code GOTO instruction with a
@@ -18,13 +18,45 @@ And here come stack frames. JVM requires compiler to generate stack frame at any
 ahead, labels that are used as jump target can be marked and stack frame can be generated when this label is reached. 
 But back jumps cannot be handled this way as byte code for these operations is already generated.
 
-Possible solutions
-------------------
+Direct jump
+-----------
 
-1. Do two-pass compilation, noting all jumps and storing their target addresses. This will allow to generate stack frames
-when they are needed. Unfortunately this doesn't work well with indirect jumps, where target address is known only in runtime.
+For each MK operation code generator creates a label and injects this label before the first byte of byte code generated 
+for this instruction. New stack frame of type `F_SAME` is generated for that location as well.
 
-2. Generate trampoline table consisting of stack frames and GOTO instructions to each block of code corresponding to MK 
-operations. Generate stack frame for each such block of code and JVM should not complain, as each will be a target of 
-GOTO instructions. Indirect jumps will be routed through trampolines. Trampoline table will add some overhead, though. 
+Code generator keeps a map between MK address and JVM byte code address (AKA label).
 
+This allows to generate simple JVM goto instruction for a label that corresponds to a target MK address.
+
+Indirect jump
+-------------
+
+JVM does not have instruction for indirect jumps per se. It has `tableswitch` instruction that allows jump based on value
+on stack. `tableswitch` contains a table of addresses to jump to based on the value of the stack.
+See more details on `tableswitch` instruction [here](https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-6.html#jvms-6.5.tableswitch)
+and [here](https://cs.au.dk/~mis/dOvs/jvmspec/ref-tableswi.html).
+
+For indirect jumps code generator will create a "trampoline" - special `tableswitch` instruction which can jump to any
+of MK addresses. MK addresses can have values starting from 0 to the number of MK operations - 1. To perform indirect jump
+code generator generates instructions to push MK address onto stack and jump to trampoline which will pick correct JVM 
+byte code address to jump to.
+
+Subroutines
+-----------
+
+### Calling subroutines 
+
+Direct and indirect `gosub` operand does not significantly differ from direct and indirect jump commands. The only difference
+is that gosub stores MK address of the next operation onto subroutine stack. Rest of the code will be identical to `goto`
+command. 
+
+### Returning from subroutines
+
+When `В/О` operation is encountered code generator will generate byte code to pop latest MK address from subroutine 
+stack onto JVM stack and then will generate jump to trampoline `tableswitch` instruction, which in turn will jump to
+correct JVM address for that MK address.     
+
+### Differences between MK calculators and generated JVM code
+
+MK calculators subroutine return address stack was limited to 5 addresses, JVM code does not have a limits on return
+address stack.
--- src/main/java/net/ninjacat/mk54/codegen/CodeGenerator.java
+++ src/main/java/net/ninjacat/mk54/codegen/CodeGenerator.java
@@ -6,6 +6,7 @@ import com.google.common.collect.ImmutableMap;
 import net.ninjacat.mk54.Mk54;
 import net.ninjacat.mk54.exceptions.ClassCreationException;
 import net.ninjacat.mk54.exceptions.UnknownOperationException;
+import net.ninjacat.mk54.opcodes.Opcode;
 import org.objectweb.asm.*;
 
 import java.util.List;
@@ -13,9 +14,9 @@ import java.util.Map;
 import java.util.stream.IntStream;
 
 import static net.ninjacat.mk54.codegen.CodeGenUtil.CLASS_DESCRIPTOR;
+import static net.ninjacat.mk54.codegen.CodeGenUtil.CLASS_NAME;
 import static net.ninjacat.mk54.opcodes.Opcode.*;
-import static org.objectweb.asm.Opcodes.ASM6;
-import static org.objectweb.asm.Opcodes.F_SAME;
+import static org.objectweb.asm.Opcodes.*;
 
 /**
  * Generates Java byte code for MK program.
@@ -25,6 +26,7 @@ class CodeGenerator {
     private static final ImmutableMap.Builder<String, OperationCodeGenerator> OPERATIONS_BUILDER = ImmutableMap.builder();
 
     private static final int MEMORY_SIZE = 15;
+    private static final String ILLEGAL_STATE_EXCEPTION = "java/lang/IllegalStateException";
 
     static {
         OPERATIONS_BUILDER
@@ -38,7 +40,7 @@ class CodeGenerator {
                 .put(SUB, MathGen::sub)
                 .put(MUL, MathGen::mul)
                 .put(DIV, MathGen::div)
-                .put(SWAP, RegisterGen::swapXy)
+                .put(Opcode.SWAP, RegisterGen::swapXy)
                 .put(TEN_TO_POWER_X, MathGen::tenToPowerX)
                 .put(E_TO_POWER_X, MathGen::eToPowerX)
                 .put(LOG10, MathGen::log)
@@ -64,7 +66,8 @@ class CodeGenerator {
                 .put(FRAC, MathGen::frac)
                 .put(MAX, MathGen::max)
                 .put(RND, MathGen::rnd)
-                .put(RUN_STOP, ControlGen::startStop);
+                .put(RUN_STOP, ControlGen::startStop)
+                .put(Opcode.GOTO, ControlGen::gotoAddr);
 
         IntStream.range(0, 10)
                 .forEach(digit -> OPERATIONS_BUILDER.put(DIGIT(digit), RegisterGen.digit(digit)));
@@ -92,23 +95,53 @@ class CodeGenerator {
         // Prepare context
         final CodeGenContext context = new CodeGenContext(operations, classWriter, executeMethod);
 
-        for (final String operation : operations) {
+        String operation = operations.get(0);
+        while (operation != null) {
             generateOperandAddressLabel(executeMethod, context);
             if (OPERATION_CODEGEN.containsKey(operation)) {
                 OPERATION_CODEGEN.get(operation).generate(executeMethod, context);
             } else {
                 throw new UnknownOperationException(operation);
             }
+            operation = context.nextOperation();
         }
 
-        executeMethod.visitInsn(Opcodes.RETURN);
-        final Label finalLabel = new Label();
+        final Label finalLabel = generateOperandAddressLabel(executeMethod, context);
         executeMethod.visitLabel(finalLabel);
+        executeMethod.visitFrame(F_SAME, 0, null, 0, null);
+        executeMethod.visitInsn(Opcodes.RETURN);
+//        generateTrampolineTable(executeMethod, context);
+//        executeMethod.visitInsn(Opcodes.RETURN);
         executeMethod.visitLocalVariable("this", CLASS_DESCRIPTOR, null, startLabel, finalLabel, 0);
         executeMethod.visitMaxs(0, 0);
         executeMethod.visitEnd();
 
         classWriter.visitEnd();
+    }
+
+    /**
+     * Generates trampoline table for indirect jumps and subroutine returns.
+     * <p>
+     * To jump to MK address push MK address on stack and execute goto to label returned to {@link CodeGenContext#getTrampolineLabel()}
+     * If MK address is invalid, IllegalStateException will be thrown
+     *
+     * @param mv      {@link MethodVisitor} for {@code Mk54.execute()} method
+     * @param context Code generation context
+     */
+    private static void generateTrampolineTable(final MethodVisitor mv, final CodeGenContext context) {
+        final Label defaultLabel = new Label();
+        mv.visitLabel(defaultLabel);
+        mv.visitFrame(F_SAME, 0, null, 0, null);
+        mv.visitTypeInsn(NEW, ILLEGAL_STATE_EXCEPTION);
+        mv.visitInsn(DUP);
+        mv.visitLdcInsn("Invalid jump operation");
+        mv.visitMethodInsn(INVOKESPECIAL, ILLEGAL_STATE_EXCEPTION, "<init>", "(Ljava/lang/String;)V", false);
+        mv.visitInsn(ATHROW);
+        mv.visitLabel(context.getTrampolineLabel());
+        mv.visitFrame(F_SAME, 0, null, 0, null);
+        mv.visitVarInsn(ALOAD, 0);
+        mv.visitFieldInsn(GETFIELD, CLASS_NAME, "indirectJumpAddress", "I");
+        mv.visitTableSwitchInsn(0, context.getOperations().size() - 1, defaultLabel, context.generateJumpTable());
     }
 
 
@@ -118,9 +151,12 @@ class CodeGenerator {
      * @param mv      {@link MethodVisitor} for generated method
      * @param context Code generating context
      */
-    private static void generateOperandAddressLabel(final MethodVisitor mv, final CodeGenContext context) {
+    private static Label generateOperandAddressLabel(final MethodVisitor mv, final CodeGenContext context) {
         final Label opLabel = context.getLabelForAddress(context.getCurrentAddress());
         mv.visitLabel(opLabel);
+        mv.visitLineNumber(context.getCurrentAddress(), opLabel);
+        mv.visitFrame(F_SAME, 0, null, 0, null);
+        return opLabel;
     }
 
     /**
@@ -129,9 +165,10 @@ class CodeGenerator {
      * @param mv      Generated method visitor
      * @param context Code generation context
      */
+    @SuppressWarnings("unused")
     private static void fail(final MethodVisitor mv, final CodeGenContext context) {
-        mv.visitTypeInsn(Opcodes.NEW, "java/lang/RuntimeException");
-        mv.visitInsn(Opcodes.DUP);
+        mv.visitTypeInsn(NEW, "java/lang/RuntimeException");
+        mv.visitInsn(DUP);
         mv.visitLdcInsn("Error");
         mv.visitMethodInsn(Opcodes.INVOKESPECIAL, "java/lang/RuntimeException", "<init>", "(Ljava/lang/String;)V", false);
         mv.visitInsn(Opcodes.ATHROW);
