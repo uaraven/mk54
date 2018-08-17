afd828f3d69bdee857b7427927ab5a263b29a445
Oleksii Voronin <ovoronin@gmail.com>
2018-08-16 22:20:08 -0400

    Fixed #5: Added compilation from source to jar file

---
+++ src/main/java/net/ninjacat/mk54/Settings.java
@@ -0,0 +1,55 @@
+package net.ninjacat.mk54;
+
+import com.beust.jcommander.Parameter;
+
+class Settings {
+
+    @Parameter(description = "<source file>", required = true)
+    private String programFile;
+
+    @Parameter(names = {"-o", "--output"}, description = "File name for output jar file")
+    private String outputFile = null;
+
+    @Parameter(names = {"-h", "--help"}, description = "Display this help screen", help = true)
+    private boolean showHelp = false;
+
+    @Parameter(names = {"-d", "--debug"}, description = "Include debug state printouts for each MK program step", hidden = true)
+    private boolean generateDebug = false;
+
+    @Parameter(names = {"-v", "--verbose"}, description = "Display more information during compilation process")
+    private boolean verbose = false;
+
+    String getProgramFile() {
+        return this.programFile;
+    }
+
+    String getOutputFile() {
+        if (this.outputFile == null) {
+            this.outputFile = stripFileExtension(this.programFile) + ".jar";
+        }
+        return this.outputFile;
+    }
+
+    private static String stripFileExtension(final String fileName) {
+        if (fileName == null) {
+            return "";
+        }
+        final int lastIndexOf = fileName.lastIndexOf(".");
+        if (lastIndexOf == -1) {
+            return fileName; // empty extension
+        }
+        return fileName.substring(0, lastIndexOf);
+    }
+
+    boolean isShowHelp() {
+        return this.showHelp;
+    }
+
+    boolean isGenerateDebug() {
+        return this.generateDebug;
+    }
+
+    boolean isVerbose() {
+        return this.verbose;
+    }
+}
